7abd1ace4d4fb4f8a3772f5bab1fa9bd3f90bbde
Jerry Qian <jerry@qian.ninja>
2019-02-16 02:43:17 -0500

    vector building completed except for marks news sentiments

--- data_aggregation/stock_history_aggregate.py
+++ data_aggregation/stock_history_aggregate.py
@@ -3,33 +3,54 @@ from requests.exceptions import RequestException
 from contextlib import closing
 import json
 
-'''INSTALL REQUESTS >> pip install requests BeautifulSoup4'''
+#INSTALL REQUESTS >> pip install requests BeautifulSoup4
 
 def simple_get(url):
     try:
         with closing(get(url, stream=True)) as resp:
         	return resp.content
-
     except RequestException as e:
         print('Error during requests to {0} : {1}'.format(url, str(e)))
         return None
 
-def build_vector(url) {
-	json_string = simple_get(url)
-	parsed_json = json.loads(json_string)
+url = "https://api.iextrading.com/1.0/stock/aapl/chart/5y"
+json_string = simple_get(url)
+parsed_json = json.loads(json_string)
 
+def build_input_vector(day_to_predict):
+	vector = [0] * 306
+	for i in range(1, 301, 3):
+		day = (int)((day_to_predict + 3) / 3)
+		vector[i] = parsed_json[day]['low']
+		vector[i+1] = parsed_json[day]['high']
+		vector[i+2] = parsed_json[day]['close']
+
+	#Mark data here
 	
-	for 
-}
+	vector[304] = (int)(parsed_json[day_to_predict]['date'][8:10])
+	vector[305] = (int)(parsed_json[day_to_predict]['date'][5:7])
 
-url = "https://api.iextrading.com/1.0/stock/aapl/chart/1y"
-print(build_vector(url))
+	return vector
+
+def build_output_vector(day_to_predict):
+	day = len(parsed_json)-1-day_to_predict
+	return [parsed_json[day]['low'], parsed_json[day]['high'], parsed_json[day]['close']]
 
 
-print(parsed_json[1], "\n")
-print(parsed_json[3]['date'])
-print(parsed_json[2]['date'])
-print(parsed_json[1]['date'])
-print(parsed_json[1]['open'])
-print(parsed_json[1]['low'], "-", parsed_json[1]['high'])
-print(parsed_json[1]['close'])
+def create():
+	input_vec_list = []
+	output_vec_list = []
+
+	for day_to_predict in range(0, 1000):
+		input_vec = build_input_vector(day_to_predict)
+		output_vec = build_output_vector(day_to_predict)
+		#print(input_vec)
+		#print(output_vec)
+		input_vec_list.append(input_vec)
+		output_vec_list.append(output_vec)
+
+	input_output_list_tuple = (input_vec_list, output_vec_list)
+
+	return input_output_list_tuple
+
+create()
