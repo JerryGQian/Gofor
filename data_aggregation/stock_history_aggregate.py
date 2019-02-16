83649bb87031a23aa9ea9ec764d455a0c82c640f
Jerry Qian <jerry@qian.ninja>
2019-02-16 02:54:41 -0500

    fixed vector generation logic

--- data_aggregation/stock_history_aggregate.py
+++ data_aggregation/stock_history_aggregate.py
@@ -20,7 +20,7 @@ parsed_json = json.loads(json_string)
 def build_input_vector(day_to_predict):
 	vector = [0] * 306
 	for i in range(1, 301, 3):
-		day = (int)((day_to_predict + 3) / 3)
+		day = (int)((i + 3) / 3)
 		vector[i] = parsed_json[day]['low']
 		vector[i+1] = parsed_json[day]['high']
 		vector[i+2] = parsed_json[day]['close']
@@ -44,8 +44,8 @@ def create():
 	for day_to_predict in range(0, 1000):
 		input_vec = build_input_vector(day_to_predict)
 		output_vec = build_output_vector(day_to_predict)
-		#print(input_vec)
-		#print(output_vec)
+		print(input_vec)
+		print(output_vec)
 		input_vec_list.append(input_vec)
 		output_vec_list.append(output_vec)
 
