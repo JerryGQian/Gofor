9485273be51afc05234b653d6a9bdfbf40f951f1
Jerry Qian <jerry@qian.ninja>
2019-02-16 21:48:37 -0500

    website finalization frontend

--- website/js/table.js
+++ website/js/table.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	table = $('#table_id').DataTable({dom:'f'});
+	table = $('#table_id').DataTable({dom:'fl'});
 	table1 = $('#table_id1').DataTable({dom:'f'});
 	$('#table_id_filter input').removeClass('searchClass');
 	$('#table_id_filter input').addClass('mySearch');
